35c5958309c00768b682a59e2f3bbe10ced32afc
user <user@KE-C02SJ9PDFVH5s-MacBook-Pro.local>
2018-10-23 21:52:30 +0300

    Add error handlers

--- run.py
+++ run.py
@@ -1,7 +1,24 @@
+from flask import jsonify
 from app import create_app
 
 CONFIG_NAME = "development"
 app = create_app(CONFIG_NAME)
+
+
+@app.errorhandler(404)
+def content_not_found(error):
+    return jsonify({'message': "Requested url is not found"}), 404
+
+
+@app.errorhandler(405)
+def url_not_found(error):
+    return jsonify({'message': "Requested URL is invalid"}), 405
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({'message': "Internal server error"}), 500
+
 
 if __name__ == '__main__':
     app.run()
